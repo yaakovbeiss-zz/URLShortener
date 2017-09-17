7662443643c76f6db5f278808f0e5aae52c20452
Yaakov Beiss <yaakovbeiss@gmail.com>
2017-09-17 12:27:11 -0400

    fix URL

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -6,7 +6,9 @@ class ShortUrlsController < ApplicationController
     short_url = ShortUrl.find(id)
     new_view_count = short_url.views + 1
     short_url.update(views: new_view_count)
+    debugger
     redirect_to short_url.long_url
+
   end
 
 end
